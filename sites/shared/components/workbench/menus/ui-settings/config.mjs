f1b164f4550a3c6ef69dd64462522575e0ecdb1b
joostdecock <joost@joost.at>
2023-06-01 17:01:48 +0200

    chore(shared): Updated ui settings and work on render view

--- sites/shared/components/workbench/menus/ui-settings/config.mjs
+++ sites/shared/components/workbench/menus/ui-settings/config.mjs
@@ -17,4 +17,18 @@ export const loadSettingsConfig = () => ({
     dflt: 'react',
     emoji: '🚀',
   },
+  inspect: {
+    control: 4, // Show when control > 3
+    list: [0, 1],
+    choiceTitles: {
+      0: 'inspectNo',
+      1: 'inspectYes',
+    },
+    valueTitles: {
+      0: 'no',
+      1: 'yes',
+    },
+    dflt: 0,
+    emoji: '🔬',
+  },
 })
