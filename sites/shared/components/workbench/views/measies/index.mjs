5b2c1735ab226b41256a00c3fa2ad307debfc0de
Enoch Riese <enoch.riese@gmail.com>
2023-06-20 14:27:00 -0500

    don't get redirected to measies on refresh

--- sites/shared/components/workbench/views/measies/index.mjs
+++ sites/shared/components/workbench/views/measies/index.mjs
@@ -3,12 +3,26 @@ import { SetPicker, ns as setsNs } from 'shared/components/sets/set-picker.mjs'
 import { Tabs, Tab } from 'shared/components/mdx/tabs.mjs'
 import { MeasiesEditor } from './editor.mjs'
 import { useTranslation } from 'next-i18next'
+import { useToast } from 'shared/hooks/use-toast.mjs'
+
 export const ns = ['wbmeasies', ...authNs, setsNs]
 
 const tabNames = ['editCurrent', 'chooseNew']
 export const MeasiesView = ({ design, Design, settings, update }) => {
   const { t } = useTranslation(['wbmeasies'])
+  const toast = useToast()
+
   const tabs = tabNames.map((n) => t(n)).join(',')
+
+  const loadMeasurements = (set) => {
+    update.settings([
+      [['measurements'], designMeasurements(Design, set.measies)],
+      [['units'], set.imperial ? 'imperial' : 'metric'],
+    ])
+    setView('draft')
+    toast.success(t('updatedMeasurements'))
+  }
+
   return (
     <div className="m-6">
       <h1 className="max-w-6xl m-auto text-center"> {t('changeMeasies')}</h1>
